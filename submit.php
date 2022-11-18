d489a59b4d60da8e8762dd9618ef3c273c72d542
gitcuamanhne-ahihi <shuriken8668@gmail.com>
2022-11-18 15:44:44 +0700

    ok

--- submit.php
+++ submit.php
@@ -8,15 +8,15 @@ $jobarr = array("Education", "Government", "Financial", "Banking",
   "Research and contruction");
 $votearr = array("Very bad", "Bad", "Medium", "Good", "Very Good");
 
-$fullname = $_POST('fullname');
-$dateofbirth = $_POST('dateofbirth');
-$email = $_POST('email');
-$phone = $_POST('phone');
-$major = $_POST('major');
-$study = $_POST('study');
-$job = $_POST('job');
-$vote = $_POST('vote');
-$idea = $_POST('idea');
+$fullname = $_POST['fullname'];
+$dateofbirth = $_POST['dateofbirth'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$major = $_POST['major'];
+$study = $_POST['study'];
+$job = $_POST['job'];
+$vote = $_POST['vote'];
+$idea = $_POST['idea'];
 
 $conn = mysqli_connect("localhost","root","","test1118");
 
@@ -24,13 +24,9 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO user(fullname, dateofbirth, email,phone,major,study,job,vote,idea)
-VALUES ($fullname, $dateofbirth, $email,$phone,$majorarr[$major],$studyarr[$study],$jobarr[$job],$votearr[$vote],$idea)";
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+$sql = "INSERT INTO user(fullname, dateofbirth, email,phone,major,study,job,vote,idea)
+VALUES (".$fullname.",". $dateofbirth .",". $email.",".$phone.",".$major.",".$study.",".$job.",".$vote.",".$idea.")";
+mysqli_query($conn, $sql);
 
 $conn->close();
